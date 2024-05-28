e995d7ca97c4a79622da819dd6e8f4e0f8fb358d
Lorenzo <lorefaria6@gmail.com>
2024-05-27 22:47:33 -0300

    Initial commit

---
+++ src/extension.js
@@ -0,0 +1,51 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = __importStar(require("vscode"));
+function activate(context) {
+    console.log('Congratulations, your extension "my-extension" is now active!');
+    let disposable = vscode.commands.registerCommand('extension.formatCode', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const formatted = formatCode(document.getText());
+            const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(document.getText().length));
+            editor.edit(editBuilder => {
+                editBuilder.replace(fullRange, formatted);
+            });
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+function formatCode(text) {
+    // formatação simples (correção de indentação)
+    const formattedText = text.replace(/^\s+/gm, ''); // faz remove espacos em branco no inicio de cada linha
+    return formattedText;
+}
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "my-extension" is now active!');
+
+    let disposable = vscode.commands.registerCommand('extension.formatCode', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const formatted = formatCode(document.getText());
+            const fullRange = new vscode.Range(
+                document.positionAt(0),
+                document.positionAt(document.getText().length)
+            );
+
+            editor.edit(editBuilder => {
+                editBuilder.replace(fullRange, formatted);
+            });
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
+
+function formatCode(text: string): string {
+    // formatação simples (correção de indentação)
+    const formattedText = text.replace(/^\s+/gm, ''); // faz remove espacos em branco no inicio de cada linha
+    return formattedText;
+}
