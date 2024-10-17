757966ddb96838622ecab70694c4e1f4fbc66113
logirardi1 <lorefaria6@gmail.com>
2024-10-16 23:55:49 -0300

    .

---
+++ teste instancias java.java
@@ -0,0 +1,17 @@
+  public  class Horrivel{public  static void
+  main(String[]args){int   num1=10;int
+num2=20;
+if(num1<num2)   {System.out.println
+   ("num1 é menor que num2");
+} else { System.
+out.println("num1 é maior ou igual a num2"
+);}int soma=num1+num2;System.
+out.println("A    soma é: " +soma)
+;int   multiplica=num1 * num2   ; System.out.
+println  ("A multiplicação é: "+ multiplica)
+;for(int   i=0;i<5;i++){System.out.
+println("Loop i="+i);} for (  int  j=0;
+   j<3;j++){ System.out.println
+  ("Loop j= "+ j);  } while (num1<50) {
+   num1+=5;System.out.println("num1 = "+num1
+);} }}
